0459191085101352c261f53bd956265432596196
Clayton Modder <121697788+Claytonmodderr@users.noreply.github.com>
2025-09-15 22:07:51 -0300

    Create video-list.js

---
+++ video-list.js
@@ -0,0 +1,36 @@
+const videos = [
+    {
+        title: "Sinal 1",
+        url: "https://cdn3-two.vercel.app/api/play.php?v=1"
+    },
+{
+        title: "Sinal 2",
+        url: "https://cdn3-two.vercel.app/api/play.php?v=2"
+    },
+
+    {
+        title: "Sinal 3",
+        url: "https://cdn3-two.vercel.app/api/play.php?v=3"
+    },
+{
+        title: "Sinal 4",
+        url: "https://cdn3-two.vercel.app/api/play.php?v=4"
+    },
+{
+        title: "Sinal 5",
+        url: "https://cdn3-two.vercel.app/api/play.php?v=5"
+    },
+{
+        title: "Sinal 6",
+        url: "https://cdn3-two.vercel.app/api/play.php?v=6"
+    },
+
+ {
+        title: "Record TV",
+        url: "https://cdn3-two.vercel.app/api/play.php?v=7"
+    },
+    {
+        title: "Record TV HD",
+        url: "https://cdn3-two.vercel.app/api/play.php?v=8"
+    }
+ ];
