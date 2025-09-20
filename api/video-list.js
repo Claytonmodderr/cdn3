0bd7cbe9e8dee612a3e2fd8ff2ea37c307df4a1d
Clayton Modder <121697788+Claytonmodderr@users.noreply.github.com>
2025-09-20 10:33:16 -0300

    Update video-list.js

--- api/video-list.js
+++ api/video-list.js
@@ -3,27 +3,57 @@ const videos = [
         title: "Sinal 1",
         url: "https://cdn3-two.vercel.app/api/play.php?v=1"
     },
+  {
+        title: "Sinal 1 ALT",
+        url: "https://reidoscanais.life/embed/?id=fazenda17"
+    },
+
 {
         title: "Sinal 2",
         url: "https://cdn3-two.vercel.app/api/play.php?v=2"
+    },
+
+  {
+        title: "Sinal 2 ALT",
+        url: "https://reidoscanais.life/embed/?id=fazenda17-2"
     },
 
     {
         title: "Sinal 3",
         url: "https://cdn3-two.vercel.app/api/play.php?v=3"
     },
+ {
+        title: "Sinal 3 ALT",
+        url: "https://reidoscanais.life/embed/?id=fazenda17-3"
+    },
+
 {
         title: "Sinal 4",
         url: "https://cdn3-two.vercel.app/api/play.php?v=4"
     },
+ {
+        title: "Sinal 4 ALT",
+        url: "https://reidoscanais.life/embed/?id=fazenda17-4"
+    },
+
 {
         title: "Sinal 5",
         url: "https://cdn3-two.vercel.app/api/play.php?v=5"
     },
+ {
+        title: "Sinal 5 ALT",
+        url: "https://reidoscanais.life/embed/?id=fazenda17-5"
+    },
+
 {
         title: "Sinal 6",
         url: "https://cdn3-two.vercel.app/api/play.php?v=6"
     },
+ {
+        title: "Sinal 6 ALT",
+        url: "https://reidoscanais.life/embed/?id=fazenda17-6"
+    },
+
 
  {
         title: "Record TV",
