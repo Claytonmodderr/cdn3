11daf2f599868bc339b20300b7cd98d32eab3a50
Clayton Modder <121697788+Claytonmodderr@users.noreply.github.com>
2025-09-24 12:44:27 -0300

    Update play1.php

--- api/play1.php
+++ api/play1.php
@@ -1,4 +1,9 @@
- 
+<?php
+$domain = "https://cdn3-tau.vercel.app/api/";
+$url = file_get_contents($domain."canais.php?list");
+$json = json_decode($url, true);
+
+?>
 <!DOCTYPE html>
 <html lang="en" >
 <head>
@@ -9,7 +14,7 @@
 </head>
 <body>
 
-
+    
     <!-- partial:index.partial.html -->
 <style>
 		    body, html, #pps {width:100%; height:100%;  }  
@@ -21,7 +26,7 @@
             }
 		</style>
 	</head>
-	<body><div id="pps"><link href="https://static.publit.io/css/player.min.css" rel="stylesheet" /><video id="pv_jNlXozfh" class="video-js vjs-skin-colors-blue vjs-big-play-centered" controls preload="auto" poster=""  oncontextmenu="return false;" controlslist="nodownload" playsinline><source src="http://stitcher-ipv4.pluto.tv/v1/stitch/embed/hls/channel/5ddc266f80e3550009136843/master.m3u8?deviceType=unknown&deviceMake=unknown&deviceModel=unknown&deviceVersion=unknown&appVersion=unknown&deviceLat=90&deviceLon=0&deviceDNT=TARGETOPT&deviceId=PSID&advertisingId=PSID&us_privacy=1YNY&profileLimit=&profileFloor=&embedPartner=" type="application/x-mpegURL"><p class="vjs-no-js"> Vizionarea nu poate fi redata!. Deoarece nu poti reda, te rugam sa actualizezi broweserul la unul actual. <a href="https://browsehappy.com/"> Alege unu dintre broweserele prezentate, pe care le folosesti actual. <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFLD3DdynGlTvLyLeOyYWC9BO03e7b8eSy_g&usqp=CAU"></a></p>
+	<body><div id="pps"><link href="https://static.publit.io/css/player.min.css" rel="stylesheet" /><video id="pv_jNlXozfh" class="video-js vjs-skin-colors-blue vjs-big-play-centered" controls preload="auto" poster=""  oncontextmenu="return false;" controlslist="nodownload" playsinline><source src="<?php echo $json[$_GET['v']]["urlHLS"];?>" type="application/x-mpegURL"><p class="vjs-no-js"> Vizionarea nu poate fi redata!. Deoarece nu poti reda, te rugam sa actualizezi broweserul la unul actual. <a href="https://browsehappy.com/"> Alege unu dintre broweserele prezentate, pe care le folosesti actual. <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFLD3DdynGlTvLyLeOyYWC9BO03e7b8eSy_g&usqp=CAU"></a></p>
 
 English version: Viewing cannot be played !. Since you cannot play, please update the browser to the current one. <a href="https://browsehappy.com/"> Choose one of the featured browsers you are currently using. <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFLD3DdynGlTvLyLeOyYWC9BO03e7b8eSy_g&usqp=CAU"> </a> </video><script src="https://static.publit.io/js/player.min.js"></script><script src="https://firebasestorage.googleapis.com/v0/b/casamireasa.appspot.com/o/live.js?alt=media&token=fe11efb2-f591-4e81-a343-5d785c7a4100"></script>	    </div>
 
