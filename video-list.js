5bb30f0bbbbfad3872ef7a91f3f78feea6f3ae4a
Clayton Modder <121697788+Claytonmodderr@users.noreply.github.com>
2025-11-08 10:54:34 -0300

    Update video-list.js

--- video-list.js
+++ video-list.js
@@ -1,36 +1,65 @@
 const videos = [
     {
         title: "Sinal 1",
-        url: "https://cdn3-two.vercel.app/api/play.php?v=1"
+        url: "https://rdcanais.top/fazenda17"
     },
+   {
+        title: "Sinal 1 ALT",
+        url: "https://cdn3-xi.vercel.app/api/play.php?v=2"
+    },
+
 {
         title: "Sinal 2",
-        url: "https://cdn3-two.vercel.app/api/play.php?v=2"
+        url: "https://rdcanais.top/fazenda17-2"
+    },
+
+{
+        title: "Sinal 2 ALT",
+        url: "https://cdn3-xi.vercel.app/api/play.php?v=4"
     },
 
     {
         title: "Sinal 3",
-        url: "https://cdn3-two.vercel.app/api/play.php?v=3"
+        url: "https://rdcanais.top/fazenda17-3"
     },
+ {
+        title: "Sinal 3 ALT",
+        url: "https://cdn3-xi.vercel.app/api/play.php?v=5"
+    },
+
 {
         title: "Sinal 4",
-        url: "https://cdn3-two.vercel.app/api/play.php?v=4"
+        url: "https://rdcanais.top/fazenda17-4"
     },
+{
+        title: "Sinal 4 ALT",
+        url: "https://cdn3-xi.vercel.app/api/play.php?v=6"
+    },
+
 {
         title: "Sinal 5",
-        url: "https://cdn3-two.vercel.app/api/play.php?v=5"
+        url: "https://rdcanais.top/fazenda17-5"
     },
 {
+        title: "Sinal 5  ALT",
+        url: "https://cdn3-xi.vercel.app/api/play.php?v=7"
+    },
+
+{
         title: "Sinal 6",
-        url: "https://cdn3-two.vercel.app/api/play.php?v=6"
+        url: "https://rdcanais.top/fazenda17-6"
+    },
+{
+        title: "Sinal 6 ALT",
+        url: "https://cdn3-xi.vercel.app/api/play.php?v=8"
     },
 
  {
         title: "Record TV",
-        url: "https://cdn3-two.vercel.app/api/play.php?v=7"
+        url: "https://cdn3-xi.vercel.app/api/play.php?v=0"
     },
     {
         title: "Record TV HD",
-        url: "https://cdn3-two.vercel.app/api/play.php?v=8"
+        url: "https://cdn3-xi.vercel.app/api/play.php?v=0"
     }
  ];
