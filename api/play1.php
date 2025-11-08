062d6b1dcc50d8c692f0c0a0102f939f3de2f62c
Clayton Modder <121697788+Claytonmodderr@users.noreply.github.com>
2025-11-08 10:58:14 -0300

    Update play1.php

--- api/play1.php
+++ api/play1.php
@@ -1,5 +1,5 @@
 <?php
-$domain = "https://cdn3-tau.vercel.app/api/";
+$domain = "https://cdn3-xi.vercel.app/api/";
 $url = file_get_contents($domain."canais.php?list");
 $json = json_decode($url, true);
 
