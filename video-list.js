3f6ebb17736e9bfe1a438727731d81dac3488a46
Clayton Modder <121697788+Claytonmodderr@users.noreply.github.com>
2025-11-24 18:27:00 -0300

    Update video-list.js

--- video-list.js
+++ video-list.js
@@ -3,58 +3,51 @@ const videos = [
         title: "Sinal 1",
         url: "https://rdcanais.top/fazenda17"
     },
-   {
+    {
         title: "Sinal 1 ALT",
         url: "https://playertv.net/e/?v=sinal1"
     },
-
-{
+    {
         title: "Sinal 2",
         url: "https://rdcanais.top/fazenda17-2"
     },
-
-{
+    {
         title: "Sinal 2 ALT",
-        url: "https://playertv.net/e/?v=sinal2
+        url: "https://playertv.net/e/?v=sinal2"
     },
-
     {
         title: "Sinal 3",
         url: "https://rdcanais.top/fazenda17-3"
     },
- {
+    {
         title: "Sinal 3 ALT",
         url: "https://playertv.net/e/?v=sinal3"
     },
-
-{
+    {
         title: "Sinal 4",
         url: "https://rdcanais.top/fazenda17-4"
     },
-{
+    {
         title: "Sinal 4 ALT",
         url: "https://playertv.net/e/?v=sinal4"
     },
-
-{
+    {
         title: "Sinal 5",
         url: "https://playertv.net/e/?v=sinal5"
     },
-{
-        title: "Sinal 5  ALT",
+    {
+        title: "Sinal 5 ALT",
         url: "https://playertv.net/e/?v=sinal5"
     },
-
-{
+    {
         title: "Sinal 6",
         url: "https://rdcanais.top/fazenda17-6"
     },
-{
+    {
         title: "Sinal 6 ALT",
         url: "https://playertv.net/e/?v=sinal6"
     },
-
- {
+    {
         title: "Record TV",
         url: "https://cdn3-xi.vercel.app/api/play.php?v=0"
     },
@@ -62,4 +55,4 @@ const videos = [
         title: "Record TV HD",
         url: "https://cdn3-xi.vercel.app/api/play.php?v=0"
     }
- ];
+];
